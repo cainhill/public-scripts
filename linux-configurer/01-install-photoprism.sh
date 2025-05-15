ea00b43770301bcf5d3ae53ecfe673dd9b8c05f2
Cain Hill <cainhill@gmail.com>
2025-05-15 18:28:37 +1000

    Create 01-install-photoprism.sh

---
+++ linux-configurer/01-install-photoprism.sh
@@ -0,0 +1,11 @@
+# TODO: Install Portainer using CachyOS Installer
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
+
+sudo systemctl status docker
+
+sudo docker volume create portainer_data
+
+sudo docker run -d -p 8000:8000 -p 9443:9443 – portainer –restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:lts
